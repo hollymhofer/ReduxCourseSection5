ded8ce0909de30e3a2c3f6751b3b48e7c82063a6
AEIENG\hhofer <hhofer@aeieng.com>
2017-07-18 17:49:32 -0400

    - api key is not working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
-const API_KEY = '69d6ad8c9f0e1072e94d32933d2095f4';
+import axios from 'axios';
+
+const API_KEY = '18a8d930a1cc2140ab6bc395e1570b9b';
+const ROOT_URL = `http://openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) { //action creator
+  const url =  `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
+
