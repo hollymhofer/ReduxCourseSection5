81fda01f2ac3c411cd84baedf28cdee846fd6d38
AEIENG\hhofer <hhofer@aeieng.com>
2017-07-21 16:46:24 -0400

    finished section 5

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 const API_KEY = '18a8d930a1cc2140ab6bc395e1570b9b';
-const ROOT_URL = `http://openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) { //action creator
   const url =  `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
+  console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
